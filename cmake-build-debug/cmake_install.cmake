07f32f428cfd8feec6c1a433162290c7c5a60b87
Evgeniy Dimitriev <dimitrievevgeniy@mail.ru>
2022-12-06 21:07:09 +0300

    fixed this FUCKING RANG

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/iwaly/CLionProjects/for_semyon
+# Install script for directory: /home/iwaly/for_semyon
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/iwaly/CLionProjects/for_semyon/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/iwaly/for_semyon/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-
+// фукнция транспонирования матрицы, просто проходимся по матрице
+// только вместо [i][j] ставим [j][i] и выводим через printf("d",Matrix[i][j])
+// тут "d" значит  что мы выводим число формата int
 void transpose(int **Matrix, int lines, int columns){
     for (int i = 0; i < columns; ++i)
     {
@@ -14,6 +16,7 @@ void transpose(int **Matrix, int lines, int columns){
         printf("\n");
     }
 }
+// Получение миноров
 int **get_matrix(int **Matrix, int size, int less_size){
     int ** res = (int **)malloc((size - 1) * sizeof(int *));
     for (int i = 0; i < size - 1; ++i)
@@ -27,62 +30,132 @@ int **get_matrix(int **Matrix, int size, int less_size){
             }
             res[i - 1][k] = Matrix[i][j];
         }
+
     }
+
     return res;
+
 }
+
+//рекурсивное вычисление дискриминанта
 int determinant(int **Matrix, int size){
     if (size == 1)
         return Matrix[0][0];
     if (size == 2)
         return (Matrix[1][1] * Matrix[0][0]) - (Matrix[0][1] * Matrix[1][0]);
+    //ans - определитель, ans - коэффицент перед дискриминантом минора, он меняет знак так как находисят в степени
     int ans = 0, sig = 1;
+    //матрица вычисляется разложением по строке и столбцу
     for (int i = 0; i < size; ++i){
         ans += (sig * Matrix[0][i] * determinant(get_matrix(Matrix, size, i),size - 1));
         sig *= -1;
     }
     return ans;
 }
-int get_rank(int ** Matrix, int lines, int columns){
-    const double EPS = 1E-9;
+// меняем местами элементы
+void swap(int **Matrix, int line1, int line2,int column)
+{
+    for (int i = 0; i < column; i++)
+    {
+        int temp = Matrix[line1][i];
+        Matrix[line1][i] = Matrix[line2][i];
+        Matrix[line2][i] = temp;
+    }
+}
 
-    int rank;
-    if(lines>columns)rank = lines;
-    else rank = columns;
-    char line_used[lines];
-    for (int i=0; i<columns; ++i) {
-        int j;
-        for (j=0; j<lines; ++j)
-            if (!line_used[j] && abs(Matrix[j][i]) > EPS)
-                break;
-        if (j == lines)
-            --rank;
-        else {
-            line_used[j] = true;
-            for (int p=i+1; p<columns; ++p)
-                Matrix[j][p] /= Matrix[j][i];
-            for (int k=0; k<lines; ++k)
-                if (k != j && abs (Matrix[k][i]) > EPS)
-                    for (int p=i+1; p<columns; ++p)
-                        Matrix[k][p] -= Matrix[j][p] * Matrix[k][i];
+//нахождение ранга матрицы
+int get_rank(int **Matrix, int lines, int columns)
+{
+    int rank = columns;
+
+    for (int row = 0; row < rank; row++)
+    {
+        // Прежде чем мы перейдем к текущей строке 'row', мы сделаем
+        // так чтобы mat[row][0],....mat[row][row-1]
+        // были равны 0.
+
+        // Диагональный элемент не равен нулю
+        if (Matrix[row][row])
+        {
+            for (int col = 0; col < lines; col++)
+            {
+                if (col != row)
+                {
+                    // Это делает все значения текущего столбца равными 0,
+                    // кроме 'mat[row][row]'
+                    double res = (double)Matrix[col][row] /Matrix[row][row];
+                    for (int i = 0; i < rank; i++)
+                        Matrix[col][i] -= res * Matrix[row][i];
+                }
+            }
         }
+
+            // Диагональный элемент уже равен нулю. Два случая возникать:
+            //1) Если под ним есть строка с ненулевым
+            // значением, затем меняем местами эту строку с той строкой
+            // и обработайте её
+            // 2) Если все элементы в текущем столбце меньше
+            // mat[r][row] равны 0, затем удаляем этот столбец
+            // заменив его на последний столбец и
+            // уменьшив количество столбцов на 1.
+        else
+        {
+            bool reduce = true;
+
+            /* Найдём ненулевой элемент в текущем
+                колонка  */
+            for (int i = row + 1; i < lines;  i++)
+            {
+                // Поменяем местами строку с ненулевым элементом
+                // с этой строкой.
+                if (Matrix[i][row])
+                {
+                    swap(Matrix, row, i, rank);
+                    reduce = false;
+                    break ;
+                }
+            }
+
+            // Если мы не нашли ни одной строки с ненулевым
+            // элемент в текущем столбце, значит все
+            // значения в этом столбце равны 0.
+            if (reduce)
+            {
+                // уменьшаем количество столбцов
+                rank--;
+
+                // скопируем последний столбец сюда
+                for (int i = 0; i < lines; i ++)
+                    Matrix[i][row] = Matrix[i][rank];
+            }
+
+            // Обработаем эту строку еще раз
+            row--;
+        }
+
+        // если нужно увидеть промежутчной результат, разкоментит эти строки
+        // display(mat, R, C);
+        // printf("\n");
     }
     return rank;
 }
 int main() {
     int lines, columns, **Matrix;
 
+    //ввод количества столбцов и строк
     printf("Type the matrix lines:\n");
     scanf("%d", &lines);
     printf("Type the matrix columns:\n");
     scanf("%d", &columns);
 
+    //выделяем память под строки
     Matrix = (int **)malloc(lines * sizeof(int *));
 
-
+    // каждой строке выделяем место для столбцов
     for (int i = 0; i < lines; ++i)
         Matrix[i] = (int *)malloc(columns * sizeof(int));
 
-
+    //ввод элементов матрицы
     for (int i = 0; i < lines; ++i)
     {
         for (int j = 0; j < columns; ++j)
@@ -91,6 +164,7 @@ int main() {
             scanf("%d", &Matrix[i][j]);
         }
     }
+    //вывод введёной матрицы
     for (int i = 0; i < lines; ++i)
     {
         for (int j = 0; j < columns; ++j)
@@ -99,7 +173,7 @@ int main() {
         }
         printf("\n");
     }
-
+    //функции
     transpose(Matrix, lines, columns);
     printf("%d\n", determinant(Matrix,lines));
     printf("%d\n", get_rank(Matrix,lines,columns));
